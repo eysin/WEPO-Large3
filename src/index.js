30b140e67ddc10535c6ca8bf5f224d169d69e4e4
Eyþór Örn Aanes Hafliðason <eyloto@gmail.com>
2020-03-04 23:18:48 +0000

    Shards-react up and running along with navbar

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import '../styles/site.css';
 import '../styles/site.less';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('app'));
