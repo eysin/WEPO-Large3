543630777643d51727b7d6c20ca714353d2aeeea
Eyþór Örn Aanes Hafliðason <eyloto@gmail.com>
2020-03-05 21:55:53 +0000

    More cart functionality added

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -6,6 +6,7 @@ import { getProducts } from '../../services/productService';
 
 const Cart = (props) => {
   const [ cart, setCart ] = useState([]);
+  const [ blindUpdate, updateState] = useState();
   
   useEffect(() => {
 	let localCart = window.localStorage.getItem("cart");
@@ -15,11 +16,9 @@ const Cart = (props) => {
 		for(let bubble in localCart){
 			tempCart.push(localCart[bubble])
 		}
-		console.log(tempCart)
 	}
     setCart(tempCart)
-  }, []);
-  console.log(cart)
+  }, [blindUpdate]);
   return (
 		<Container>
 			<h1>Cart</h1>
@@ -27,18 +26,20 @@ const Cart = (props) => {
 				<thead>
 					<tr>
 					<th>Bubble Name</th>
-					<th>Price</th>
 					<th>Count</th>
+					<th>Price</th>
 					<th>Total</th>
+					<th>Remove</th>
 					</tr>
 				</thead>
 				<tbody>
 					{cart.map((value, index) => (
 					<tr key={index}>
 						<td>{value.name}</td>
-						<td>{value.count}</td>
-						<td>{value.price}</td>
-						<td>{value.count * value.price}</td>
+						<td><button className="btn btn-link" onClick={() => changeCount(-1, index, cart, updateState)}>-</button>{value.count}<button className="btn btn-link"onClick={() => changeCount(1, index, cart, updateState)}>+</button></td>
+						<td>{value.price}$</td>
+						<td>{value.count * value.price}$</td>
+						<td><button className="btn btn-link" onClick={() => remove(index, cart, updateState)}><i className="fa fa-trash"/></button></td>
 					</tr>
 					))}
 				
@@ -53,6 +54,35 @@ const Cart = (props) => {
 		</Container>
         
     )
+}
+const changeCount = (count, id, cart, setCart) => {
+	cart[id].count += count;
+	if(cart[id].count === 0){
+		remove(id, cart, setCart);
+	}
+	else{
+		updateCart(cart, setCart)
+	}
+}
+const remove = (id, cart, setCart) => {
+	delete cart[id];
+	updateCart(cart, setCart);
+
+}
+
+const updateCart = (cart, setCart) => {
+	let tempStorage = {};
+	for(let i in cart){
+		tempStorage[i] = cart[i];
+	}
+	if(tempStorage === {}){
+		window.localStorage.removeItem("cart")
+	}
+	else{
+		window.localStorage.setItem("cart", JSON.stringify(tempStorage))
+	}
+	setCart(cart)
+	
 }
 
 export default Cart;
