ccb16c58ff671741bcb17cb0acbc1c5b5dc9734e
Eyþór Örn Aanes Hafliðason <eyloto@gmail.com>
2020-03-05 23:35:56 +0000

    Final Version

--- src/components/Orders/index.js
+++ src/components/Orders/index.js
@@ -9,16 +9,63 @@ const Orders = () => {
   const [user, setUser] = useState([])
   
   useEffect(() => {
-    let userString 
-
-  })
+    let tempUser = window.localStorage.getItem("user");
+    if(tempUser !== null){
+      tempUser = JSON.parse(tempUser);
+      setUser(tempUser)
+      console.log(tempUser)
+      fetch("http://localhost:3500/api/orders/" + tempUser.telephone)
+      .then(res =>{
+        if(res.status === 200){
+          res.json()
+          .then(json =>{
+            setOrders(json)
+          })
+        }
+      })
+    }
     
   }, []);
+  
+
+  const addToCart = (order) => {
+    console.log(order)
+    window.localStorage.setItem("cart", JSON.stringify(order))
+    window.location.href = "/cart";
+  }
+
   return (
     <Container>
       <div id="products-text">
-        <h1>Our Products</h1>
+        <h1>Previous orders for {user.name}</h1>
       </div>
+      {orders.map((order, index)=>
+        <div key={index}>          
+        <table className="table">
+            <thead>
+              <tr>
+              <th>Bubble Name</th>
+              <th>Count</th>
+              <th>Price</th>
+              <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.Order.map((value, nindex) => (
+              <tr key={index + nindex}>
+                <td>{value.name}</td>
+                <td>{value.count}</td>
+                <td>{value.price}$</td>
+                <td>{value.count * value.price}$</td>
+              </tr>
+              ))}
+            
+            </tbody>
+          </table>
+              <input type="button" className="btn btn-success" value="Add Order To Cart" onClick={() => addToCart(order.Order)}/>
+        </div>
+      )}
+     
     </Container>
     )
 }
